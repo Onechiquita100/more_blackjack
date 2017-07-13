b72862492fc8c421b87768e376d5e33818fd5281
Onechiquita100 <alysia_chang2002@yahoo.com>
2017-07-13 16:07:13 -0700

    init commit

---
+++ main.js
@@ -0,0 +1,40 @@
+/*
+   Implement a Blackjack hand value calculator.
+
+   Open up the `index.html` file and your console
+   to watch the assertions pass as you write your code.
+
+   Also remember, that the parameter `hand` will be an array, so
+   you'll need to parse through that first before you can start to
+   write your logic.
+*/
+let cardValue[2, 3, 4, 5, 6, 7, 8, 9, 10];
+let cardName ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+let "K" = 10;
+let "Q" = 10;
+let "J" = 10;
+let "Ace" = 1 || 11;
+
+function handValue (hand) {
+  let total = 0;
+
+
+if (value =< 21) {
+  return true;
+}
+else {
+  return false;
+}
+
+
+  return value;
+}
+
+
+/* -----  Hints ------
+
+1..10   ==> Worth face value (1 = 1, 4 = 4, etc)
+K, Q, J ==> Worth 10
+A       ==> Worth 1 or 11
+
+*/
